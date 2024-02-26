74093fc11b7c038421f4845cc2297585edd13323
Ferios <exferios@gmail.com>
2024-02-26 19:52:07 +0300

    Rearranged code and improved output color range

--- src/main.cpp
+++ src/main.cpp
@@ -52,13 +52,16 @@ typedef struct FfmpegContext
   int stream_idx;
 }TFfmpegCtx;
 
-static const char characters[] = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+/* We keep additional cpaces at the end for cases when rounding results in a larger value */
+static const char characters[] = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'.   ";
 static const char* font_name = "SpaceMono-Regular.ttf";
-
-int init_ffmpeg(TFfmpegCtx* ffmpegctx)
+static const int tile_size = 4;
+static const float coeff = 256 / (float)(sizeof(characters) - 3);
+static int init_ffmpeg(TFfmpegCtx* ffmpegctx, char* file_name)
 {
   int ret = 0;
 
+    ffmpegctx->file = file_name;
   ffmpegctx->codec = nullptr;
   ffmpegctx->stream = nullptr;
   ffmpegctx->decframe = nullptr;
@@ -66,21 +69,22 @@ int init_ffmpeg(TFfmpegCtx* ffmpegctx)
   ffmpegctx->end_of_stream = false;
   ffmpegctx->got_image = 0;
 
-  // Open file context
+    /* Open file context */
   ffmpegctx->input_ctx = nullptr;
   if (avformat_open_input(&(ffmpegctx->input_ctx), ffmpegctx->file, nullptr, nullptr) < 0) 
   {
       std::cerr << "Avformat open error: " << ret;
       return 2;
   }
-  // Get input stream info
+
+    /* Get input stream info */
   if (avformat_find_stream_info(ffmpegctx->input_ctx, nullptr) < 0) 
   {
       std::cerr << "Find stream info error: " << ret;
       return 2;
   }
 
-  // Detect video stream
+    /* Detect video stream */
   ffmpegctx->stream_idx = av_find_best_stream(ffmpegctx->input_ctx, AVMEDIA_TYPE_VIDEO, 
                                                 -1, -1, (const AVCodec**)(&(ffmpegctx->codec)), 0);
   if (ffmpegctx->stream_idx < 0) 
@@ -106,7 +110,7 @@ int init_ffmpeg(TFfmpegCtx* ffmpegctx)
         return 1;
     }
 
-  // Open decoder context
+    /* Open decoder context*/
   if (avcodec_open2(ffmpegctx->codec_ctx, ffmpegctx->codec, nullptr) < 0) {
       std::cerr << "Av codec open error: " << ret;
       return 2;
@@ -114,7 +118,10 @@ int init_ffmpeg(TFfmpegCtx* ffmpegctx)
 
   ffmpegctx->pkt = av_packet_alloc();
 
-  // Print video info
+    /* Allocate space for frame decoder */
+    ffmpegctx->decframe = av_frame_alloc();
+
+    /* Print video info */
   cout
       << "format: " << ffmpegctx->input_ctx->iformat->name << endl
       << "codec: "  << ffmpegctx->codec->name << endl
@@ -128,14 +135,7 @@ int init_ffmpeg(TFfmpegCtx* ffmpegctx)
   return 0;
 }
 
-static void cleanup(int exitcode)
-{
-    TTF_Quit();
-    SDL_Quit();
-    exit(exitcode);
-}
-
-int init_sdl(TSDLContext *sdlctx)
+static int init_sdl(TSDLContext *sdlctx)
 {
     sdlctx->window = SDL_CreateWindow("", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
     if(sdlctx->window == NULL)
@@ -156,6 +156,141 @@ int init_sdl(TSDLContext *sdlctx)
                   
     return 0;
 }
+static void cleanup(int exitcode, TSDLContext *sdlctx, TFfmpegCtx* ffmpegctx)
+{
+    /* De-init ffmpeg */
+    if (ffmpegctx->decframe)
+    {
+        av_frame_free(&(ffmpegctx->decframe));
+    }
+
+    if (ffmpegctx->codec_ctx)
+    {
+        avcodec_close(ffmpegctx->codec_ctx);
+    }
+
+    if (ffmpegctx->input_ctx)
+    {
+        avformat_close_input(&(ffmpegctx->input_ctx));
+    }
+
+    /* De-init SDL */
+    if (sdlctx->fc_font)
+    {
+        FC_FreeFont(sdlctx->fc_font);
+    }
+    SDL_Quit();
+    exit(exitcode);
+}
+static void update_window_size(FC_Font* fc_font, AVStream* stream, SDL_Window *window)
+{
+    const int winheight = (stream->codecpar->height) * 1.77;
+    const int winwidth = (stream->codecpar->width / 4) * FC_GetWidth(fc_font, "%s", "c");
+    if (winwidth != WIDTH || winheight!= HEIGHT)
+    {
+        SDL_SetWindowSize(window, winwidth, winheight);
+    }
+}
+static int get_frame(TFfmpegCtx* ffmpegctx)
+{
+    int ret = 0;
+
+    /* Read next packet */
+    if (!ffmpegctx->end_of_stream) 
+    {
+        ret = av_read_frame(ffmpegctx->input_ctx, ffmpegctx->pkt);
+        if (ret < 0 && ret != AVERROR_EOF) {
+            std::cerr << "read frame error: " << ret;
+            return -1;
+        }
+
+        if (ret == 0 && ffmpegctx->pkt->stream_index != ffmpegctx->stream_idx)
+        {
+            av_packet_unref(ffmpegctx->pkt);
+            return 1;
+        }
+        
+        ffmpegctx->end_of_stream = (ret == AVERROR_EOF);
+    }
+    
+    /* Make a final dummy packet to end loop */
+    if (ffmpegctx->end_of_stream) 
+    {
+        ffmpegctx->pkt = av_packet_alloc();
+        ffmpegctx->pkt->data = nullptr;
+        ffmpegctx->pkt->size = 0;
+    }
+    
+    /* Decode packet and see if we have a frame */
+    ret = avcodec_send_packet(ffmpegctx->codec_ctx, ffmpegctx->pkt);
+    if (ret < 0) {
+        fprintf(stderr, "Error sending a packet for decoding\n");
+        return -1;
+    }
+
+    ffmpegctx->got_image = 1;
+    ret = avcodec_receive_frame(ffmpegctx->codec_ctx, ffmpegctx->decframe);
+    if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
+    {
+        ffmpegctx->got_image = 0;
+        av_packet_unref(ffmpegctx->pkt);
+        return 1;
+    }
+    else if (ret < 0) {
+        ffmpegctx->got_image = 0;
+        fprintf(stderr, "Decoder error\n");
+        return -1;
+    }
+
+    av_packet_unref(ffmpegctx->pkt);
+    return 0;
+}
+
+static void handle_frame(SDL_Renderer *renderer, FC_Font* fc_font, AVFrame* frame, enum AVColorRange color_range, char* ascii_buffer)
+{
+    /* Reset viewport */
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+    SDL_RenderClear(renderer);
+    int character_index;
+
+    /* Average values of pixels within a tile of a frame and store them in an array */
+    for (int heightIdx = 0; heightIdx < frame->height; heightIdx += tile_size) 
+    {
+        for (int widthIdx = 0, cellId = 0; widthIdx < frame->width; widthIdx += tile_size, cellId++) 
+        {
+            /* For simplicity tiling is hardcoded */
+            /* We extract luma data from Y channel of YUV420p */
+            const uint8_t* line1 = &frame->data[0][heightIdx * frame->linesize[0] + widthIdx];
+            const uint8_t* line2 = line1 + frame->linesize[0];
+            const uint8_t* line3 = line2 + frame->linesize[0];
+            const uint8_t* line4 = line3 + frame->linesize[0];
+            const float tile_luma = (line1[0] + line1[1] + line1[2] + line1[3] +
+                                    line2[0] + line2[1] + line2[2] + line2[3] +
+                                    line3[0] + line3[1] + line3[2] + line3[3] +
+                                    line4[0] + line4[1] + line4[2] + line4[3]) / 
+                                    (tile_size * tile_size);
+
+            /* Check if the color range is limited and translate to full */
+            if (color_range != AVCOL_RANGE_JPEG)
+            {
+                const float luma_full = (tile_luma > 16) ? ((tile_luma - 16) / 220) * 255 : 0; 
+                character_index = round(luma_full / coeff);
+            } 
+            else
+            {
+                character_index = round(tile_luma / coeff);
+            }
+
+            ascii_buffer[cellId] = characters[character_index];
+        }
+
+        /* Draw line by line to control lineheight */
+        FC_Draw(fc_font, renderer, 0, heightIdx * 1.75, "%s", ascii_buffer);
+    }
+
+    /* Update viewport */
+    SDL_RenderPresent(renderer);
+}
 
 int main(int argc, char *argv[])
 {
@@ -164,45 +299,41 @@ int main(int argc, char *argv[])
 
     int ret = 0;
     bool done = false;
-    char* ascii_art = nullptr;
-    const float coeff = 255 / (float)(sizeof(characters) - 1);
+    char* ascii_buffer = nullptr;
 
     if (argc < 2) 
     {
         std::cout << "Usage: ascii_player <file>" << std::endl;
         return 1;
     }
-    ffmpegctx.file = argv[1];
 
-    if (init_ffmpeg(&ffmpegctx))
+    if (init_ffmpeg(&ffmpegctx, argv[1]))
     {
+        cleanup(1, &sdlctx, &ffmpegctx);
         return -1;
     }
 
-    init_sdl(&sdlctx);
-
-    /* Allocate space for frame decoder */
-    ffmpegctx.decframe = av_frame_alloc();
+    if (init_sdl(&sdlctx))
+    {
+        cleanup(1, &sdlctx, &ffmpegctx);
+        return -1;
+    }
     
     /* Calculate frame time */
     const auto frametime = std::chrono::milliseconds(1000 / (int)av_q2d(ffmpegctx.stream->r_frame_rate));
 
     /* Allocate ASCII frame */
-    ascii_art = (char*)malloc(ffmpegctx.stream->codecpar->width / 4 + 1);
+    ascii_buffer = (char*)malloc(ffmpegctx.stream->codecpar->width / 4 + 1);
 
-    /* Update window size */
-    const int winheight = (ffmpegctx.stream->codecpar->height) * 1.77;
-    const int winwidth = (ffmpegctx.stream->codecpar->width / 4) * FC_GetWidth(sdlctx.fc_font, "%s", "c");
-    if (winwidth != WIDTH || winheight!= HEIGHT)
-    {
-        SDL_SetWindowSize(sdlctx.window, winwidth, winheight);
-    }
+    /* Update window size now that we know content dimensions */
+    update_window_size(sdlctx.fc_font, ffmpegctx.stream, sdlctx.window);
 
     do
     {
         auto now = std::chrono::system_clock::now();
         auto start = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch());
 
+        /* Detect quit attempt or button press */
         while (SDL_PollEvent(&sdlctx.event))
         {
           switch (sdlctx.event.type) 
@@ -216,86 +347,21 @@ int main(int argc, char *argv[])
           }
         }
 
-        /* Read next packet */
-        if (!ffmpegctx.end_of_stream) 
+        /* Decode next frame from file if there are any */
+        ret = get_frame(&ffmpegctx);
+        if (ret > 0)
         {
-            ret = av_read_frame(ffmpegctx.input_ctx, ffmpegctx.pkt);
-            if (ret < 0 && ret != AVERROR_EOF) {
-                std::cerr << "read frame error: " << ret;
-                break;
-            }
-            
-
-            if (ret == 0 && ffmpegctx.pkt->stream_index != ffmpegctx.stream_idx)
-            {
-                av_packet_unref(ffmpegctx.pkt);
-                continue;
-            }
-            
-            ffmpegctx.end_of_stream = (ret == AVERROR_EOF);
-        }
-        
-        /* Make a final dummy packet to end loop */
-        if (ffmpegctx.end_of_stream) 
-        {
-            ffmpegctx.pkt = av_packet_alloc();
-            ffmpegctx.pkt->data = nullptr;
-            ffmpegctx.pkt->size = 0;
-        }
-        
-        /* Decode packet and see if we have a frame */
-
-        ret = avcodec_send_packet(ffmpegctx.codec_ctx, ffmpegctx.pkt);
-        if (ret < 0) {
-            fprintf(stderr, "Error sending a packet for decoding\n");
-            exit(1);
-        }
-
-        ffmpegctx.got_image = 1;
-        ret = avcodec_receive_frame(ffmpegctx.codec_ctx, ffmpegctx.decframe);
-        if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
-        {
-            ffmpegctx.got_image = 0;
-            av_packet_unref(ffmpegctx.pkt);
+            /* Not enough data to decode whole frame, try again */
             continue;
         }
-        else if (ret < 0) {
-            ffmpegctx.got_image = 0;
-            fprintf(stderr, "Error during decoding\n");
+        else if (ret < 0)
+        {
+            /* Something went wrong */
             break;
         }
 
-        av_packet_unref(ffmpegctx.pkt);
-
-        /* Reset viewport */
-        SDL_SetRenderDrawColor(sdlctx.renderer, 0x00, 0x00, 0x00, 0x00);
-        SDL_RenderClear(sdlctx.renderer);
-
-        /* Average values of pixels within a tile of a frame and store them in an array */
-        for (int heightIdx = 0; heightIdx < ffmpegctx.decframe->height; heightIdx+=4) 
-        {
-            for (int widthIdx = 0, cellId = 0; widthIdx < ffmpegctx.decframe->width; widthIdx+=4, cellId++) 
-            {
-                /* For simplicity tiling is hardcoded */
-                const uint8_t* line1 = &ffmpegctx.decframe->data[0][heightIdx * ffmpegctx.decframe->linesize[0] + widthIdx];
-                const uint8_t* line2 = line1 + ffmpegctx.decframe->linesize[0];
-                const uint8_t* line3 = line2 + ffmpegctx.decframe->linesize[0];
-                const uint8_t* line4 = line3 + ffmpegctx.decframe->linesize[0];
-                const float intensity = (line1[0] + line1[1] + line1[2] + line1[3] +
-                                        line2[0] + line2[1] + line2[2] + line2[3] +
-                                        line3[0] + line3[1] + line3[2] + line3[3] +
-                                        line4[0] + line4[1] + line4[2] + line4[3]) / 16;
-                const int character_index = intensity / coeff;
-                ascii_art[cellId] = characters[character_index];
-            }
-            ascii_art[ffmpegctx.decframe->width / 4] = '\n';
-
-            /* Draw line by line to control lineheight */
-            FC_Draw(sdlctx.fc_font, sdlctx.renderer, 0, heightIdx * 1.75, "%s", ascii_art);
-        }
-
-        /* Update viewport */
-        SDL_RenderPresent(sdlctx.renderer);
+        /* Process pixel data and render it as ASCII */
+        handle_frame(sdlctx.renderer, sdlctx.fc_font, ffmpegctx.decframe, ffmpegctx.stream->codecpar->color_range, ascii_buffer);
 
         /* Wait until we need to present next frame */
         now = std::chrono::system_clock::now();
@@ -308,18 +374,10 @@ int main(int argc, char *argv[])
     while ((!ffmpegctx.end_of_stream || ffmpegctx.got_image) && (done == false));
     
     /* Release ASCII frame */
-    free(ascii_art);
+    free(ascii_buffer);
 
-    /* De-init ffmpeg */
-    av_frame_free(&ffmpegctx.decframe);
-    avcodec_close(ffmpegctx.codec_ctx);
-    avformat_close_input(&ffmpegctx.input_ctx);
-
-    /* De-init SDL */
-    FC_FreeFont(sdlctx.fc_font);
-    cleanup(0);
+    cleanup(0, &sdlctx, &ffmpegctx);
 
     /* Not reached, but fixes compiler warnings */
     return 0;
 }
-
