7bc507c4b8ee8652c831ae99aea8c56fbe916a96
Ferios <exferios@gmail.com>
2024-02-26 14:50:26 +0300

    Integrated SDL_FontCache to vastly impore performance and control line spacing

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,9 @@ extern "C" {
 #include <libswscale/swscale.h>
 }
 
-
 #include <SDL.h>
 #include <SDL_ttf.h>
+#include "SDL_FontCache.h"
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -27,48 +27,12 @@ extern "C" {
 
 using namespace std;
 
-typedef enum {
-    RENDER_LATIN1,
-    RENDER_UTF8,
-    RENDER_UNICODE
-} RenderType;
-
-typedef enum
-{
-    TextRenderSolid,
-    TextRenderShaded,
-    TextRenderBlended
-} TextRenderMethod;
-
-typedef struct {
-    SDL_Texture *caption;
-    SDL_Rect captionRect;
-    SDL_Texture *message;
-    SDL_Rect messageRect;
-} Scene;
-
 typedef struct SDLContext
 {
     SDL_Window *window;
     SDL_Renderer *renderer;
-    TTF_Font *font;
-    SDL_Surface *text;
-    Scene scene;
-    int ptsize;
-    SDL_Color white;
-    SDL_Color black;
-    SDL_Color *forecol;
-    SDL_Color *backcol;
     SDL_Event event;
-    TextRenderMethod rendermethod;
-    RenderType rendertype;
-    int renderstyle;
-    int outline;
-    int hinting;
-    int kerning;
-    int wrap;
-    int dump;
-    char *message, string[128];
+    FC_Font* fc_font;
 }TSDLContext;
 
 typedef struct FfmpegContext
@@ -90,87 +54,6 @@ typedef struct FfmpegContext
 
 static const char characters[] = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
 static const char* font_name = "SpaceMono-Regular.ttf";
-
-
-void save_frame_ascii(AVFrame *pFrame, int width, int height, int iFrame)
-{
-    FILE *pFile;
-    char szFilename[32];
-    const float char_count = (float)(sizeof(characters)-1);
-    
-    // Open file
-    sprintf(szFilename, "frame%d.txt", iFrame);
-    pFile=fopen(szFilename, "w");
-    if(pFile==NULL)
-        return;
-
-    // Write ascii
-    for (int heightIdx = 0; heightIdx < height; heightIdx++) 
-    {
-      for (int widthIdx = 0; widthIdx < width; widthIdx++) 
-      {
-          const int intensity = pFrame->data[0][heightIdx * pFrame->linesize[0] + widthIdx];
-          const int character_index = intensity / (255 / char_count);
-
-          fputc(characters[character_index], pFile);
-      }
-      fputc('\n', pFile);
-    }
-    // Close file
-    fclose(pFile);
-}
-
-void save_frame_luminance(AVFrame *pFrame, int width, int height, int iFrame)
-{
-    FILE *pFile;
-    char szFilename[32];
-    int  y;
-
-    // Open file
-    sprintf(szFilename, "frame%d.ppm", iFrame);
-    pFile=fopen(szFilename, "wb");
-    if(pFile==NULL)
-        return;
-
-    // Write header
-    fprintf(pFile, "P6\n%d %d\n255\n", width, height);
-
-    // Write ascii
-    for (int heightIdx = 0; heightIdx < height; heightIdx++) 
-    {
-      for (int widthIdx = 0; widthIdx < width; widthIdx++) 
-      {
-        fwrite(pFrame->data[0]+heightIdx*pFrame->linesize[0] + widthIdx, 1, 1, pFile);
-        fwrite(pFrame->data[0]+heightIdx*pFrame->linesize[0] + widthIdx, 1, 1, pFile);
-        fwrite(pFrame->data[0]+heightIdx*pFrame->linesize[0] + widthIdx, 1, 1, pFile);
-      }
-    }
-    // Close file
-    fclose(pFile);
-}
-
-void save_frame_rgb(AVFrame *pFrame, int width, int height, int iFrame)
-{
-    FILE *pFile;
-    char szFilename[32];
-    int  y;
-
-    // Open file
-    sprintf(szFilename, "frame%d.ppm", iFrame);
-    pFile=fopen(szFilename, "wb");
-    if(pFile==NULL)
-        return;
-
-    // Write header
-    fprintf(pFile, "P6\n%d %d\n255\n", width, height);
-
-    // Write pixel data
-    for(y=0; y<height; y++)
-        fwrite(pFrame->data[0]+y*pFrame->linesize[0], 1, width*3, pFile);
-
-    // Close file
-    fclose(pFile);
-}
 
 int init_ffmpeg(TFfmpegCtx* ffmpegctx)
 {
@@ -245,18 +128,6 @@ int init_ffmpeg(TFfmpegCtx* ffmpegctx)
   return 0;
 }
 
-
-static void draw_scene(SDL_Renderer *renderer, Scene *scene)
-{
-    /* Clear the background to background color */
-    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-    SDL_RenderClear(renderer);
-
-    SDL_RenderCopy(renderer, scene->caption, NULL, &scene->captionRect);
-    SDL_RenderCopy(renderer, scene->message, NULL, &scene->messageRect);
-    SDL_RenderPresent(renderer);
-}
-
 static void cleanup(int exitcode)
 {
     TTF_Quit();
@@ -264,191 +135,25 @@ static void cleanup(int exitcode)
     exit(exitcode);
 }
 
-int draw_text(char* message, TSDLContext *sdlctx)
-{
-    switch (sdlctx->rendertype) {
-        case RENDER_LATIN1:
-            switch (sdlctx->rendermethod) {
-            case TextRenderSolid:
-                if (sdlctx->wrap) {
-                    sdlctx->text = TTF_RenderText_Solid_Wrapped(sdlctx->font, message, *sdlctx->forecol, 0);
-                } else {
-                    sdlctx->text = TTF_RenderText_Solid(sdlctx->font, message, *(sdlctx->forecol));
-                }
-                break;
-            case TextRenderShaded:
-                if (sdlctx->wrap) {
-                    sdlctx->text = TTF_RenderText_Shaded_Wrapped(sdlctx->font, message, *(sdlctx->forecol), *(sdlctx->backcol), 0);
-                } else {
-                    sdlctx->text = TTF_RenderText_Shaded(sdlctx->font, message, *(sdlctx->forecol), *(sdlctx->backcol));
-                }
-                break;
-            case TextRenderBlended:
-                if (sdlctx->wrap) {
-                    sdlctx->text = TTF_RenderText_Blended_Wrapped(sdlctx->font, message, *(sdlctx->forecol), 0);
-                } else {
-                    sdlctx->text = TTF_RenderText_Blended(sdlctx->font, message, *(sdlctx->forecol));
-                }
-                break;
-            }
-            break;
-
-        case RENDER_UTF8:
-            switch (sdlctx->rendermethod) {
-            case TextRenderSolid:
-                if (sdlctx->wrap) {
-                    sdlctx->text = TTF_RenderUTF8_Solid_Wrapped(sdlctx->font, message, *(sdlctx->forecol), 0);
-                } else {
-                    sdlctx->text = TTF_RenderUTF8_Solid(sdlctx->font, message, *(sdlctx->forecol));
-                }
-                break;
-            case TextRenderShaded:
-                if (sdlctx->wrap) {
-                    sdlctx->text = TTF_RenderUTF8_Shaded_Wrapped(sdlctx->font, message, *(sdlctx->forecol), *(sdlctx->backcol), 0);
-                } else {
-                    sdlctx->text = TTF_RenderUTF8_Shaded(sdlctx->font, message, *(sdlctx->forecol), *(sdlctx->backcol));
-                }
-                break;
-            case TextRenderBlended:
-                if (sdlctx->wrap) {
-                    sdlctx->text = TTF_RenderUTF8_Blended_Wrapped(sdlctx->font, message, *(sdlctx->forecol), 0);
-                } else {
-                    sdlctx->text = TTF_RenderUTF8_Blended(sdlctx->font, message, *(sdlctx->forecol));
-                }
-                break;
-            }
-            break;
-
-        case RENDER_UNICODE:
-        {
-            Uint16 *unicode_text = SDL_iconv_utf8_ucs2(message);
-            switch (sdlctx->rendermethod) {
-            case TextRenderSolid:
-                if (sdlctx->wrap) {
-                    sdlctx->text = TTF_RenderUNICODE_Solid_Wrapped(sdlctx->font, unicode_text, *(sdlctx->forecol), 0);
-                } else {
-                    sdlctx->text = TTF_RenderUNICODE_Solid(sdlctx->font, unicode_text, *(sdlctx->forecol));
-                }
-                break;
-            case TextRenderShaded:
-                if (sdlctx->wrap) {
-                    sdlctx->text = TTF_RenderUNICODE_Shaded_Wrapped(sdlctx->font, unicode_text, *(sdlctx->forecol), *(sdlctx->backcol), 0);
-                } else {
-                    sdlctx->text = TTF_RenderUNICODE_Shaded(sdlctx->font, unicode_text, *(sdlctx->forecol), *(sdlctx->backcol));
-                }
-                break;
-            case TextRenderBlended:
-                if (sdlctx->wrap) {
-                    sdlctx->text = TTF_RenderUNICODE_Blended_Wrapped(sdlctx->font, unicode_text, *(sdlctx->forecol), 0);
-                } else {
-                    sdlctx->text = TTF_RenderUNICODE_Blended(sdlctx->font, unicode_text, *(sdlctx->forecol));
-                }
-                break;
-            }
-            SDL_free(unicode_text);
-        }
-        break;
-    }
-    
-    if (sdlctx->text == NULL) {
-        SDL_Log("Couldn't render text: %s\n", SDL_GetError());
-        TTF_CloseFont(sdlctx->font);
-        cleanup(2);
-    }
-    
-    sdlctx->scene.messageRect.x = 0;
-    sdlctx->scene.messageRect.y = 0;
-    sdlctx->scene.messageRect.w = sdlctx->text->w;
-    sdlctx->scene.messageRect.h = sdlctx->text->h;
-    sdlctx->scene.message = SDL_CreateTextureFromSurface(sdlctx->renderer, sdlctx->text);
-
-    /* Update window size to fit the surface */
-    if (sdlctx->text->w != WIDTH || sdlctx->text->h != HEIGHT)
-    {
-        SDL_SetWindowSize(sdlctx->window, sdlctx->text->w, sdlctx->text->h);
-    }
-
-    SDL_FreeSurface(sdlctx->text);
-    sdlctx->text = nullptr;
-
-    draw_scene(sdlctx->renderer, &(sdlctx->scene));
-    SDL_DestroyTexture(sdlctx->scene.message);
-    sdlctx->scene.message = nullptr;
-
-    return 0;
-}
-
 int init_sdl(TSDLContext *sdlctx)
 {
-    sdlctx->text = NULL;
-    sdlctx->wrap = 1;
-    sdlctx->white = { 0xFF, 0xFF, 0xFF, 0 };
-    sdlctx->black = { 0x00, 0x00, 0x00, 0 };
-
-    /* Look for special execution mode */
-    sdlctx->dump = 0;
-    /* Look for special rendering types */
-    sdlctx->rendermethod = TextRenderShaded;
-    sdlctx->renderstyle = TTF_STYLE_NORMAL;
-    sdlctx->rendertype = RENDER_LATIN1;
-    sdlctx->outline = 0;
-    sdlctx->hinting = TTF_HINTING_NORMAL;
-    sdlctx->kerning = 1;
-    /* Default is black and white */
-    sdlctx->forecol = &sdlctx->white;
-    sdlctx->backcol = &sdlctx->black;
-
-    /* Initialize the TTF library */
-    if (TTF_Init() < 0) {
-        SDL_Log("Couldn't initialize TTF: %s\n",SDL_GetError());
-        SDL_Quit();
-        return(2);
+    sdlctx->window = SDL_CreateWindow("", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+    if(sdlctx->window == NULL)
+    {
+        SDL_Log("Failed to create window.\n");
+        return 2;
     }
 
-    /* Open the font file with the requested point size */
-    sdlctx->ptsize = DEFAULT_PTSIZE;
-    sdlctx->font = TTF_OpenFont(font_name, sdlctx->ptsize);
-    if (sdlctx->font == NULL) {
-        SDL_Log("Couldn't load %d pt font from %s: %s\n",
-                    sdlctx->ptsize, font_name, SDL_GetError());
-        cleanup(2);
-    }
-    TTF_SetFontStyle(sdlctx->font, sdlctx->renderstyle);
-    TTF_SetFontOutline(sdlctx->font, sdlctx->outline);
-    TTF_SetFontKerning(sdlctx->font, sdlctx->kerning);
-    TTF_SetFontHinting(sdlctx->font, sdlctx->hinting);
-
-    /* Create a window */
-    if (SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &sdlctx->window, &sdlctx->renderer) < 0) {
-        SDL_Log("SDL_CreateWindowAndRenderer() failed: %s\n", SDL_GetError());
-        cleanup(2);
-    }
-
-    /* Show which font file we're looking at */
-    SDL_snprintf(sdlctx->string, sizeof(sdlctx->string), "Font file: \n%s", font_name);  /* possible overflow */
-    switch (sdlctx->rendermethod) {
-    case TextRenderSolid:
-        sdlctx->text = TTF_RenderText_Solid_Wrapped(sdlctx->font, sdlctx->string, *sdlctx->forecol, 0);
-        break;
-    case TextRenderShaded:
-        sdlctx->text = TTF_RenderText_Shaded_Wrapped(sdlctx->font, sdlctx->string, *sdlctx->forecol, *sdlctx->backcol, 0);
-        break;
-    case TextRenderBlended:
-        sdlctx->text = TTF_RenderText_Blended_Wrapped(sdlctx->font, sdlctx->string, *sdlctx->forecol, 0);
-        break;
-    }
-    if (sdlctx->text != NULL) {
-        sdlctx->scene.captionRect.x = 4;
-        sdlctx->scene.captionRect.y = 4;
-        sdlctx->scene.captionRect.w = sdlctx->text->w;
-        sdlctx->scene.captionRect.h = sdlctx->text->h;
-        sdlctx->scene.caption = SDL_CreateTextureFromSurface(sdlctx->renderer, sdlctx->text);
-        SDL_FreeSurface(sdlctx->text);
+    sdlctx->renderer = SDL_CreateRenderer(sdlctx->window, -1, SDL_RENDERER_PRESENTVSYNC);
+    if(sdlctx->renderer == NULL)
+    {
+        SDL_Log("Failed to create renderer.\n");
+        return 3;
     }
     
-    SDL_Log("Font is generally %d big, and string is %d big\n",
-                        TTF_FontHeight(sdlctx->font), sdlctx->text->h);
-                        
+    sdlctx->fc_font = FC_CreateFont();  
+    FC_LoadFont(sdlctx->fc_font, sdlctx->renderer, font_name, 9, FC_MakeColor(255,255,255,255), TTF_STYLE_NORMAL);
+                  
     return 0;
 }
 
@@ -457,7 +162,6 @@ int main(int argc, char *argv[])
     TSDLContext sdlctx = {0};
     TFfmpegCtx ffmpegctx = {0};
 
-    int counter = 0;
     int ret = 0;
     bool done = false;
     char* ascii_art = nullptr;
@@ -484,7 +188,15 @@ int main(int argc, char *argv[])
     const auto frametime = std::chrono::milliseconds(1000 / (int)av_q2d(ffmpegctx.stream->r_frame_rate));
 
     /* Allocate ASCII frame */
-    ascii_art = (char*)malloc((ffmpegctx.stream->codecpar->width / 4 + 1) * (ffmpegctx.stream->codecpar->height / 4));
+    ascii_art = (char*)malloc(ffmpegctx.stream->codecpar->width / 4 + 1);
+
+    /* Update window size */
+    const int winheight = (ffmpegctx.stream->codecpar->height) * 1.77;
+    const int winwidth = (ffmpegctx.stream->codecpar->width / 4) * FC_GetWidth(sdlctx.fc_font, "%s", "c");
+    if (winwidth != WIDTH || winheight!= HEIGHT)
+    {
+        SDL_SetWindowSize(sdlctx.window, winwidth, winheight);
+    }
 
     do
     {
@@ -555,11 +267,14 @@ int main(int argc, char *argv[])
 
         av_packet_unref(ffmpegctx.pkt);
 
+        /* Reset viewport */
+        SDL_SetRenderDrawColor(sdlctx.renderer, 0x00, 0x00, 0x00, 0x00);
+        SDL_RenderClear(sdlctx.renderer);
+
         /* Average values of pixels within a tile of a frame and store them in an array */
-        counter = 0;
         for (int heightIdx = 0; heightIdx < ffmpegctx.decframe->height; heightIdx+=4) 
         {
-            for (int widthIdx = 0; widthIdx < ffmpegctx.decframe->width; widthIdx+=4) 
+            for (int widthIdx = 0, cellId = 0; widthIdx < ffmpegctx.decframe->width; widthIdx+=4, cellId++) 
             {
                 /* For simplicity tiling is hardcoded */
                 const uint8_t* line1 = &ffmpegctx.decframe->data[0][heightIdx * ffmpegctx.decframe->linesize[0] + widthIdx];
@@ -570,16 +285,17 @@ int main(int argc, char *argv[])
                                         line2[0] + line2[1] + line2[2] + line2[3] +
                                         line3[0] + line3[1] + line3[2] + line3[3] +
                                         line4[0] + line4[1] + line4[2] + line4[3]) / 16;
-                                        
                 const int character_index = intensity / coeff;
-        
-                ascii_art[counter++] = characters[character_index];
+                ascii_art[cellId] = characters[character_index];
             }
-            ascii_art[counter++] = '\n';
+            ascii_art[ffmpegctx.decframe->width / 4] = '\n';
+
+            /* Draw line by line to control lineheight */
+            FC_Draw(sdlctx.fc_font, sdlctx.renderer, 0, heightIdx * 1.75, "%s", ascii_art);
         }
-        
-        /* Draw ASCII art */
-        draw_text(ascii_art, &sdlctx);
+
+        /* Update viewport */
+        SDL_RenderPresent(sdlctx.renderer);
 
         /* Wait until we need to present next frame */
         now = std::chrono::system_clock::now();
@@ -600,16 +316,7 @@ int main(int argc, char *argv[])
     avformat_close_input(&ffmpegctx.input_ctx);
 
     /* De-init SDL */
-    if (sdlctx.text)
-    {
-        SDL_FreeSurface(sdlctx.text);
-    }
-    if (sdlctx.scene.message)
-    {
-        SDL_DestroyTexture(sdlctx.scene.message);
-    }
-    TTF_CloseFont(sdlctx.font);
-    SDL_DestroyTexture(sdlctx.scene.caption);
+    FC_FreeFont(sdlctx.fc_font);
     cleanup(0);
 
     /* Not reached, but fixes compiler warnings */
